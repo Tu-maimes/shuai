620554e9a2b0cbdd79fe3543b997caf0b2d8319f
shuaishuai <Administrator@2013-20160307YL>
2017-08-31 17:18:41 +0800

    更新版本上一版本过滤器失败,不能完成过滤任务1.2

--- shuai-web/src/main/java/com/zhiyou100/video/web/interceptor/CustomInterceptor.java
+++ shuai-web/src/main/java/com/zhiyou100/video/web/interceptor/CustomInterceptor.java
@@ -2,10 +2,10 @@ package com.zhiyou100.video.web.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.zhiyou100.video.model.User;
 import com.zhiyou100.video.model.admin;
 
 public class CustomInterceptor implements HandlerInterceptor {
@@ -24,12 +24,21 @@ public class CustomInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object obj) throws Exception {
-		admin ad = (admin)req.getSession().getAttribute("admin");
-		if(ad != null){
-			return true;
+		String str = req.getRequestURI();
+		if(str.endsWith("action")){
+			admin ad = (admin)req.getSession().getAttribute("admin");
+			if(ad != null){
+				return true;
+			}else
+			res.sendRedirect(req.getContextPath()+"/login.jsp");
+			return false;
+		}else{
+			User us = (User)req.getSession().getAttribute("_front_user");
+			if(us != null){
+				return true;
+			}
+			res.sendRedirect(req.getContextPath()+"/front/user.do");
+			return false;
 		}
-		res.sendRedirect(req.getContextPath()+"/login.jsp");
-		return false;
 	}
-
 }
